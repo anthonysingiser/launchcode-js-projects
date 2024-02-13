e2cc7de81db414420e444a019cd64b1c0e4ac84f
chrysanthemum <asingiser@gmail.com>
2024-02-13 11:58:43 -0500

    studio for unit testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,9 @@
 
 let launchcode = {
-  
+  organization: 'nonprofit',
+  percentageCoolEmployees: 100,
+  executiveDirector: 'Jeff',
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"]
 }
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,18 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode object", function(){
+  test('value of the key "organization" is "nonprofit"', function(){
+    expect(launchcode.organization).toEqual('nonprofit')
+  })
+  test('value of the key "percentageCoolEmployees" is 100', function(){
+    expect(launchcode.percentageCoolEmployees).toEqual(100)
+  })
+  test('value of the key "executiveDirector" is "Jeff"', function(){
+    expect(launchcode.executiveDirector).toEqual('Jeff')
+  })
+  test('value of the key "programsOffered" is an array containing: "Web Development" "Data Analysis" and "Liftoff"', function(){
+    expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"])
+  })
 
-  // Write your unit tests here!
-  
 });
