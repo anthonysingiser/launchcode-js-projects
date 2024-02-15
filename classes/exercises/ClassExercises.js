437ccb04d9f51f53ee4e3de091bc67f752c03350
chrysanthemum <asingiser@gmail.com>
2024-02-15 11:42:20 -0500

    class exeercises

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,7 +1,17 @@
 // Define your Book class here:
-
-
+class Book {
+    constructor(title, author, copyright, isbn, totalPages, checkedCount, discarded) {
+        this.title = title;
+        this.author = author;
+        this.copyright = copyright;
+        this.isbn = isbn;
+        this.totalPages = totalPages;
+        this.checkedCount = checkedCount;
+        this.discarded = discarded;
+    }
+}
 // Define your Manual and Novel classes here:
+
 
 
 // Declare the objects for exercises 2 and 3 here:
