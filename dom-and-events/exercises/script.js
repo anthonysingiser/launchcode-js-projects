5c213c005f23d1b43e4cc0d572301e0cac3c4dc2
Anthony <asingiser@gmail.com>
2024-03-17 11:40:47 -0400

    finish exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,27 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function(){
+        let response = window.confirm("Are you sure you want to abort the mission?");
+        if (response) {
+            paragraph.innerHTML = "Mission aborted! Space shittle returning home.";
+        } else {
+            paragraph.innerHTML = "Crisis Averted! The Mission continues!";
+        }
+    })
+
 }
 
 window.addEventListener("load", init);
