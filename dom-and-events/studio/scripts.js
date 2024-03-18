d3c0b2667d30b38bf39551e396f93c7f42da9fb6
Anthony <asingiser@gmail.com>
2024-03-18 13:44:01 -0400

    finish studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -11,6 +11,25 @@ function init () {
     const downButton = document.getElementById('down');
     const rightButton = document.getElementById('right');
     const leftButton = document.getElementById('left');
+    const rocketImage = document.getElementById('rocket');
+
+    function moveRocket(direction, amount) {
+        var property = direction === 'up' || direction === 'down' ? 'bottom' : 'left';
+        var current = parseInt(rocketImage.style[property], 10) || 0;
+        var change = direction === 'up' || direction === 'right' ? amount : -amount;
+        var newPropValue = current + change;
+    
+        // Check if the new position is within the backgroundImage
+        if (property === 'left') {
+            if (newPropValue >= 0 && newPropValue <= shuttleBackground.offsetWidth - rocketImage.offsetWidth) {
+                rocketImage.style[property] = newPropValue + 'px';
+            }
+        } else { // 'bottom'
+            if (newPropValue >= 0 && newPropValue <= shuttleBackground.offsetHeight - rocketImage.offsetHeight) {
+                rocketImage.style[property] = newPropValue + 'px';
+            }
+        }
+    }
 
     takeOffButton.addEventListener('click', function() {
         const response = window.confirm('Confirm that the shuttle is ready for takeoff.');
@@ -25,6 +44,9 @@ function init () {
         flightStatus.innerText = 'The shuttle has landed';
         shuttleBackground.style.backgroundColor = 'green';
         spaceShuttleHeight.innerText = 0;
+
+        rocketImage.style.bottom = '0px';
+        rocketImage.style.left = '0px';
     });
     abortButton.addEventListener('click', function () {
         const response = window.confirm('Confirm that you want to abort the mission.');
@@ -32,6 +54,9 @@ function init () {
             flightStatus.innerText = 'Mission aborted.';
             shuttleBackground.style.backgroundColor = 'green';
             spaceShuttleHeight.innerText = 0;
+
+            rocketImage.style.bottom = '0px';
+            rocketImage.style.left = '0px';
         }
     });
     upButton.addEventListener('click', function() {
@@ -40,6 +65,18 @@ function init () {
     downButton.addEventListener('click', function() {
         spaceShuttleHeight.innerText = Number(spaceShuttleHeight.innerText) - 10000;
     });
+    rightButton.addEventListener('click', function() {
+        moveRocket('right', 10);
+    })
+    leftButton.addEventListener('click', function() {
+        moveRocket('left', 10);
+    })
+    upButton.addEventListener('click', function() {
+        moveRocket('up', 10);
+    })
+    downButton.addEventListener('click', function() {
+        moveRocket('down', 10);
+    })
 
 }
 
